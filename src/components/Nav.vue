888fa720aa4ce8d516f74daba419671eab54fb91
Jose Salazar <98187087+Josesimplecoding@users.noreply.github.com>
2022-03-11 12:40:12 -0600

    Initial commit

---
+++ src/components/Nav.vue
@@ -0,0 +1,8 @@
+<template>
+    <div class="text-green-200 bg-green-900">
+        <div class="container flex items-center justify-center mx-auto">
+            <h1 class="py-8 text-3xl font-thin tracking-tighter">
+                Helpful<span class="font-normal">Advice</span></h1>
+        </div>
+    </div>
+</template>
